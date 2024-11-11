3f01940060db5bd2a23fe7f0b4f8fbdadb13294d
Cristiano Magno <161599641+cristianosolutions@users.noreply.github.com>
2024-11-11 09:21:25 -0300

    Update authMiddleware.js

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
-    
+
     if (!token) return res.status(401).json({ error: 'Acesso negado' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
@@ -13,3 +13,5 @@ const authMiddleware = (req, res, next) => {
 };
 
 module.exports = authMiddleware;
+
+teste
